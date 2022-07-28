c9b861a881d5c49809d90c94e3f017ca31525e20
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-28 11:44:40 +0900

    rating 버튼으로 증가/감소

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0728_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0728_1/MainActivity.java
@@ -3,12 +3,44 @@ package kr.hs.emirim.s2125.mirim_project_0728_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    RatingBar ratingSmall, ratingMedium, ratingBasic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingSmall = findViewById(R.id.rating_small);
+        ratingMedium = findViewById(R.id.rating_medium);
+        ratingBasic = findViewById(R.id.rating_basic);
+
+        Button btnUp = findViewById(R.id.btn_inc);
+        Button btnDown = findViewById(R.id.btn_dec);
+
+        btnUp.setOnClickListener(btnListener);
+        btnDown.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.btn_inc:
+                    ratingSmall.setRating(ratingSmall.getRating()+ratingSmall.getStepSize());
+                    ratingMedium.setRating(ratingMedium.getRating()+ratingMedium.getStepSize());
+                    ratingBasic.setRating(ratingBasic.getRating()+ratingBasic.getStepSize());
+                    break;
+                case R.id.btn_dec:
+                    ratingSmall.setRating(ratingSmall.getRating()-ratingSmall.getStepSize());
+                    ratingMedium.setRating(ratingMedium.getRating()-ratingMedium.getStepSize());
+                    ratingBasic.setRating(ratingBasic.getRating()-ratingBasic.getStepSize());
+                    break;
+            }
+        }
+    };
 }
